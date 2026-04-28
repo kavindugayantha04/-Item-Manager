40466993e21bb9c133e372cb4f0929046e6a3bf9
Kavindu Gayantha <kavindugayantha178@gmail.com>
2026-04-28 11:24:33 +0530

    second comment

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://item-manager-production-9318.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
